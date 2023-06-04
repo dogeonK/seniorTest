247605c4c0b95db5dd4a7d32414492170dc12875
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-06-05 05:41:44 +0900

    [UPDATE] 비동기처리 수정

--- modelImport/asgi.py
+++ modelImport/asgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-
+from daphne import get_default_application
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "modelImport.settings")
 
-application = get_asgi_application()
+django_application = get_asgi_application()
+daphne_application = get_default_application()
+
+application = daphne_application
